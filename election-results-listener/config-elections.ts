ba42cafad94d73131391b34bcb0fc448b0126d06
Elad Heller <eladhller@gmail.com>
2022-10-31 22:53:27 +0200

    replace code bucket

--- election-results-listener/config-elections.ts
+++ election-results-listener/config-elections.ts
@@ -39,7 +39,7 @@ const electionsConfig: Record<number, ElectionsConfig> = {
   },
   19: {
     algorithm: 'baderOffer',
-    csvUrl: 'https://israel-elections-1.s3.eu-west-3.amazonaws.com/19.csv',
+    csvUrl: 'https://israel-elections-code-bucket.s3.eu-west-3.amazonaws.com/19.csv',
     // csvUrl: 'http://www.votes-19.gov.il/files/expc.csv',
     blockPercentage: 0.02,
     agreements: [['שס', 'ג'], ['מחל', 'טב'], ['ץ', 'כן'], ['מרצ', 'צפ'], ['אמת', 'פה'], ['ד', 'ו'], ['זך', 'צק']],
@@ -53,25 +53,25 @@ const electionsConfig: Record<number, ElectionsConfig> = {
   },
   17: {
     algorithm: 'baderOffer',
-    csvUrl: 'https://israel-elections-1.s3.eu-west-3.amazonaws.com/Result17.csv',
+    csvUrl: 'https://israel-elections-code-bucket.s3.eu-west-3.amazonaws.com/Result17.csv',
     blockPercentage: 0.02,
     agreements: [['שס', 'ג'], ['מחל', 'ל'], ['ו', 'ד'], ['מרצ', 'אמת']],
   },
   16: {
     algorithm: 'baderOffer',
-    csvUrl: 'https://israel-elections-1.s3.eu-west-3.amazonaws.com/16.csv',
+    csvUrl: 'https://israel-elections-code-bucket.s3.eu-west-3.amazonaws.com/16.csv',
     blockPercentage: 0.015,
     agreements: [['שס', 'ג'], ['יש', 'רק'], ['ל', 'כן'], ['ב', 'ם'], ['אמת', 'מרצ'], ['ו', 'ד']],
   },
   15: {
     algorithm: 'baderOffer',
-    csvUrl: 'https://israel-elections-1.s3.eu-west-3.amazonaws.com/15.csv',
+    csvUrl: 'https://israel-elections-code-bucket.s3.eu-west-3.amazonaws.com/15.csv',
     blockPercentage: 0.015,
     agreements: [['אמת', 'מרצ'], ['יט', 'כן'], ['שס', 'ג'], ['ם', 'ל'], ['מחל', 'ב'],
       ['עם', 'ו'], ['יש', 'רק'], ['פ', 'פז'], ['פה', 'הד']],
   },
   14: {
-    // csvUrl: 'https://israel-elections-1.s3.eu-west-3.amazonaws.com/14.csv',
+    // csvUrl: 'https://israel-elections-code-bucket.s3.eu-west-3.amazonaws.com/14.csv',
     algorithm: 'baderOffer',
     blockPercentage: 0.015,
     agreements: [['אמת', 'מרצ'], ['ג', 'ב'], ['ו', 'עם'], ['מחל', 'שס'], ['ג', 'ב'], ['הד', 'כן']],
